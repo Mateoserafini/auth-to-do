3f9ef48bd7349613dc4e84dcd210fc206ae69501
trawssy <matuserafini@gmail.com>
2024-04-24 13:25:26 -0300

    generacion de token, encriptado de contraseñas y guardado async

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,26 +1,35 @@
 import User from '../models/user.model.js';
+import bcript from 'bcryptjs';
+import { createAccessToken } from '../libs/jwt.js';
 
 export const register = async (req,res) => {
     const { email, password, username } = req.body;
 
     try{
+        
+        const passwordHash = await bcript.hash(password, 10);
         const newUser = new User ({
             username,
             email,
-            password
+            password: passwordHash
         });
 
         const userSaved = await newUser.save();
+        const token = await createAccessToken({id: userSaved._id});
 
-        res.json(userSaved);
+        res.cookie('token', token);
+        res.json({id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createAt: userSaved.createdAt,
+            updateAt: userSaved.updatedAt,
+        }); 
 
     }catch(error){
-
-        console.log(error)
-
+        res.status(500).json({message: error.message})
     }
 };
 
 export const login = (req,res) => {
-    console.log(req.body)
+    console.log(req.body);
 };
