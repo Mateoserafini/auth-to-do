62cece1836ed61c1a0bce5404f4b59b1cf78f58f
trawssy <matuserafini@gmail.com>
2024-04-24 21:37:39 -0300

    Descubri los backTics

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import { connectDB } from './db.js'
 
 connectDB();
 app.listen(3000)
-console.log('server en el puerto 3000')
+console.log(`server en el puerto 3000 ${'http://localhost:3000'}`);
+//back tics agr + }
